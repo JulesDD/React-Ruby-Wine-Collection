5d7ea5ef76f60928819516ee74e509cfe1aea968
Jules Douglas <jules.douglas@aceproject.space>
2020-01-13 23:35:45 -0600

    updated index.jsx

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Home from "../components/Home"
+import React from "react"
+import { render } from "react-dom"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import App from "../components/App"
 
-export default(
-  <Router>
-    <Switch>
-      <Route path="/" exact compontent={Home} />
-    </Switch>
-  </Router>
-);
+render(
+  <App />
+)
