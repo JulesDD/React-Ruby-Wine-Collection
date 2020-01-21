2f216b7b2876b1797b07109e246c510a404c0954
Jules Douglas <jules.douglas@aceproject.space>
2020-01-20 20:29:02 -0600

    create price controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'price/index'
+      get 'price/create'
+      get 'price/show'
+      get 'price/destroy'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      get 'recipes/create'
+      get 'recipes/show'
+      get 'recipes/destroy'
+    end
+  end
   root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
