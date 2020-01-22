3f05b9660ac2e87f8d3a3aded414a4409af92db1
Jules Douglas <jules.douglas@aceproject.space>
2020-01-22 16:06:23 -0600

    installed faker gem to create prices

--- app/controllers/api/v1/price_controller.rb
+++ app/controllers/api/v1/price_controller.rb
@@ -1,15 +1,43 @@
 class Api::V1::PriceController < ApplicationController
   def index
-    price = Price.all.order(create_at: :desc) #this will grab all the prices in the database.
-    render json: price #send the list of prices as a JSON response with render.
+    price = Price.all.order(create_at: :desc) # This will grab all the prices in the database.
+    render json: price # Send the list of prices as a JSON response with render.
   end
 
   def create
+    price = Price.create!(price_params)
+    if price
+      render json: price
+    else
+      render json: price.errors
+    end
   end
 
   def show
+    if price
+      render json: price
+    else
+      render json: price.errors
+    end
   end
 
+  # Using Ruby’s safe navigation operator &., which avoids nil errors when calling a method.
+  # This let’s you delete a recipe only if it exists, then send a message as a response.
   def destroy
+    price&.destroy
+    render json: { message: 'Price deleted' }
+  end
+
+  private
+  # Method where you whitelisted your controller parameters to prevent wrong or malicious content from getting into your database.
+  # In this case, you are permitting a name, image, ingredients, and instruction parameter for valid use of the create method.
+  def price_params
+    params.permit(:name, :image, :price)
+  end
+
+  # This method uses ActiveRecord's find method to find a recipe whose if matches the id provided in params.
+  # This then assigns it to an instance variable called @price
+  def price
+    @price ||= Price.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+9.times do |i|
+  Collection.create(
+    name: "Wine #{i+1}",
+    price: Faker::Number.decimal(l_digits: 3)
+  )
+end
