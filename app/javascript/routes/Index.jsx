fc9c0ae31a3023281a9a14c3668e92c01d3cf886
Jules Douglas <jules.douglas@aceproject.space>
2020-01-22 21:22:56 -0600

    Created new component and routes

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Collection from "../components/Collection";
+import NewCollection from "../components/NewCollection";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/collection" exact component={Collection} />
+      <Route path="/collections" exact component={Collections} />
       <Route path="/collection/:id" exact component={Collection} />
+      <Route path="/collection" exact component={NewCollection} />
     </Switch>
   </Router>
 );
